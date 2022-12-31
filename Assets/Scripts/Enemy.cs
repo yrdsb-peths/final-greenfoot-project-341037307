71eb6399a941ce962a8a97ed9f40c4a295410a2a
341037307 <117941837+341037307@users.noreply.github.com>
2022-12-31 18:10:52 -0500

    added HP system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,12 @@ public class Enemy : MonoBehaviour
 
     public int maxHealth = 100;
     int currentHealth;
+    public GameObject player;
+    public Collider2D enemyCollider;
+    public Collider2D playerCollider;
+
+    private float damageRate = 1;
+    private float nextAttackTime;
 
     // Start is called before the first frame update
     void Start()
@@ -14,10 +20,22 @@ public class Enemy : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    void Update()
+    {
+        
+
+        if (enemyCollider.IsTouching(playerCollider) && Time.time >= nextAttackTime)
+        {
+            Debug.Log("We took damage");
+            player.GetComponent<PlayerHealth>().TakeDamage();
+            nextAttackTime = Time.time + 1/damageRate;
+        }
+    }
+
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-
+        // Probably add knockback sometime
         if (currentHealth <= 0)
         {
             Die();
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static int fullHP = 5;
+    public int health = fullHP;
+    
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (Image img in hearts)
+        {
+            img.sprite = emptyHeart;
+        }
+        for (int i = 0; i < health; i++)
+        {
+            hearts[i].sprite = fullHeart;
+        }
+    }
+
+    public void TakeDamage()
+    {
+        health -= 1;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("You have Died");
+        health = 5;
     }
 }
